e5825044756b4be6f9cb81ef36a6ff9dec46a518
Deusia <gzc1094917614@outlook.com>
2020-10-15 14:36:20 +0800

    完善

--- src/sc/ch3/Equation.java
+++ src/sc/ch3/Equation.java
@@ -1,68 +1,68 @@
 package sc.ch3;
 
-import java.util.*;
+import java.util.Random;
 
 public abstract class Equation {
-	
-	private final int MAX=100;
-	private final int MIN =0;
-	private int left;
-	private int right;
-	private int result;
-	private char op;
-	
-	public Equation(){
-		
-	}
 
-	public int getLeft() {
-		return left;
-	}
+    private final int MAX = 100;
+    private final int MIN = 0;
+    private int left;
+    private int right;
+    private int result;
+    private char op;
 
-	public void setLeft(int left) {
-		this.left = left;
-	}
+    public Equation() {
 
-	public int getRight() {
-		return right;
-	}
+    }
 
-	public void setRight(int right) {
-		this.right = right;
-	}
+    public int getLeft() {
+        return left;
+    }
 
-	public int getResult() {
-		return result;
-	}
+    public void setLeft(int left) {
+        this.left = left;
+    }
 
-	public void setResult(int result) {
-		this.result = result;
-	}
+    public int getRight() {
+        return right;
+    }
 
-	public char getOp() {
-		return op;
-	}
+    public void setRight(int right) {
+        this.right = right;
+    }
 
-	public void setOp(char op) {
-		this.op = op;
-	}
-	
-	private int generateRandom(int min,int max){
-		 //生成随机的整数的方法
-		Random r=new Random(max+1);
-		return r.nextInt(max-min+1);
-	}
-	
-	private boolean isBetween(int value,int min,int max){
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+
+    public char getOp() {
+        return op;
+    }
+
+    public void setOp(char op) {
+        this.op = op;
+    }
+
+    private int generateRandom(int min, int max) {
+        //生成随机的整数的方法
+        Random r = new Random();
+        return r.nextInt(max - min + 1);
+    }
+
+    private boolean isBetween(int value, int min, int max) {
         //判断运算结在某个值得范围内的方法
         if (value >= min && value <= max) {
             return true;
         } else {
             return false;
         }
-	}
-	
-	public boolean isEqual(Equation e){
+    }
+
+    public boolean isEqual(Equation e) {
         //判断两个算式是否相等的方法
         boolean b = false;
         if (this.getOp() != e.getOp()) {
@@ -72,34 +72,36 @@ public abstract class Equation {
         }
 
         return b;
-	}
-	
-	//将算式转换为字符串类型 	45+32
-	public String toString(){
-		return ""+this.left+this.op+this.right;
-	}
-	//将算式转换为字符串类型 	45+32=
-	public String toString2(){
-		String s=toString();
-		return s+"=";
-	}
-	//将算式转换为字符串类型 	45+32=87
-	public String toString3(){
-		String s=toString2();
-		return s+this.result;
-	}
-	
-	public void generateEquation(char op){
-		do{
-			left=generateRandom(MIN, MAX);
-			right=generateRandom(MIN, MAX);
-			result=calculate();
-		}while(!isBetween(result, MIN, MAX));
-		
-		this.setOp(op);
-	}		
-	
-	protected abstract int calculate();
-	
+    }
+
+    //将算式转换为字符串类型 	45+32
+    public String toString() {
+        return "" + this.left + this.op + this.right;
+    }
+
+    //将算式转换为字符串类型 	45+32=
+    public String toString2() {
+        String s = toString();
+        return s + "=";
+    }
+
+    //将算式转换为字符串类型 	45+32=87
+    public String toString3() {
+        String s = toString2();
+        return s + this.result;
+    }
+
+    public void generateEquation(char op) {
+        do {
+            left = generateRandom(MIN, MAX);
+            right = generateRandom(MIN, MAX);
+            result = calculate();
+        } while (!isBetween(result, MIN, MAX));
+
+        this.setOp(op);
+    }
+
+    protected abstract int calculate();
+
 
 }
--- src/sc/ch3/Student.java
+++ src/sc/ch3/Student.java
@@ -1,34 +1,44 @@
 package sc.ch3;
 
-public class Student {
-	private static Exercise exercise;
-	
-	
-	public Student(){
-		
-	}
-	
-	public static void printExercise(){
-		int i=0;
-		while(exercise.hasNext()){
-			i++;
-			System.out.print("("+i+")"+exercise.next().toString());
-		}
-		if (i%5==0) {
-			System.out.println();
-		}else{
-			System.out.print("\t");
-		}
-		
-		
-	}
+import java.util.ArrayList;
 
-	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
-		exercise =new Exercise();
-		exercise.generateExercise();
-		exercise.printExercise();
-		
-	}
+public class Student {
+    private static Exercise exercise;
+
+
+    public Student() {
+
+    }
+
+    public static void printExercise() {
+        int i = 0;
+        while (exercise.hasNext()) {
+            System.out.print("(" + (i + 1) + ")" + exercise.next().toString3());
+            exercise.setIndex(i++);
+
+            if (i % 5 == 0) {
+                System.out.println();
+            } else {
+                System.out.print("\t");
+            }
+        }
+        System.out.println();
+        exercise.getExercise().clear();
+
+    }
+
+    public static void main(String[] args) {
+
+        exercise = new Exercise();
+        System.out.println("--------------------------如下是混合运算习题----------------------");
+        exercise.generateExercise();
+        exercise.printExercise();
+        System.out.println("--------------------------如下是加法运算习题----------------------");
+        exercise.generateAddExercise();
+        exercise.printExercise();
+        System.out.println("--------------------------如下是减法运算习题----------------------");
+        exercise.generateSubExercise();
+        exercise.printExercise();
+    }
 
 }
